361f9680cbcb93032c778bf378b741073f1fb7f3
Petr Bunegin <bunegin.p@gmail.com>
2018-11-14 15:26:48 +0400

    progress2

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,6 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Stack;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -5,38 +8,53 @@ import java.util.regex.Pattern;
 public class Main {
     Stack<String> stack = new Stack<>();
 
-    public static void main(String[] args) {
-        String str = "4*5+(6-5)*5+6*8+(6*5+(5+4)*8+(11*4+6/5)+44-5)";
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String str = reader.readLine();
 
-        while (!str.matches("\\d+")) {
-            Pattern p = Pattern.compile("\\([-\\d\\+\\*\\/]*\\)");
-            Matcher m = p.matcher(str);
-            String res = "";
-            while (m.find()) {
-                str = str.replace(m.group(), calculate(m.group()));
+        try {
+            while (!str.matches("\\d+")) {
+                Pattern p = Pattern.compile("\\([-\\d\\+\\*\\/]*\\)");
+                Matcher m = p.matcher(str);
+                if (!str.contains("(")) {
+                    str = calculate(str);
+                    break;
+                }
+                while (m.find()) {
+                    str = str.replace(m.group(), calculate(m.group()));
+                }
             }
+        } catch (ArithmeticException e){
+            System.out.println("Ошибка, возможно вы пытались выполнить деление на ноль");
         }
         System.out.println(str);
     }
 
-    private static String calculate(String str) {
-        Pattern p = Pattern.compile("\\d+[\\*\\/]\\d+");
-        Matcher m = p.matcher(str);
-        while (m.find()) {
-            String[] value = m.group().split("[\\*\\/]");
-            Integer res = m.group().matches("\\d+[\\*]\\d+") ?
-                    new Integer(value[0]) * new Integer(value[1]) :
-                    new Integer(value[0]) / new Integer(value[1]);
-            str.replace(m.group(), String.valueOf(res));
+    //4*5/(6-5)*5+6*8+(6*5*(5+4)*8+(11*4+6/5)+44-5)
+
+    private static String calculate(String str) throws ArithmeticException{
+        str = str.replaceAll("[\\(\\)]", "");
+        while (str.contains("*") || str.contains("/")) {
+            Pattern p = Pattern.compile("\\d+[\\*\\/]\\d+");
+            Matcher m = p.matcher(str);
+            while (m.find()) {
+                String[] value = m.group().split("[\\*\\/]");
+                Integer res = m.group().matches("\\d+[\\*]\\d+") ?
+                        new Integer(value[0]) * new Integer(value[1]) :
+                        new Integer(value[0]) / new Integer(value[1]);
+                str = str.replace(m.group(), String.valueOf(res));
+            }
         }
-        p = Pattern.compile("\\d+[-\\+]\\d+");
-        m = p.matcher(str);
-        while (m.find()) {
-            String[] value = m.group().split("[-\\+]");
-            Integer res = m.group().matches("\\d+[\\+]\\d+") ?
-                    new Integer(value[0]) + new Integer(value[1]) :
-                    new Integer(value[0]) - new Integer(value[1]);
-            str.replace(m.group(), String.valueOf(res));
+        while (!str.matches("\\d+")) {
+            Pattern p = Pattern.compile("\\d+[-\\+]\\d+");
+            Matcher m = p.matcher(str);
+            while (m.find()) {
+                String[] value = m.group().split("[-\\+]");
+                Integer res = m.group().matches("\\d+[\\+]\\d+") ?
+                        new Integer(value[0]) + new Integer(value[1]) :
+                        new Integer(value[0]) - new Integer(value[1]);
+                str = str.replace(m.group(), String.valueOf(res));
+            }
         }
         return str;
     }
