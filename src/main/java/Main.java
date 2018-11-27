f8bc2c2129c185f3be9e9a6af0f521a278cc1469
Petr Bunegin <bunegin.p@gmail.com>
2018-11-27 22:05:02 +0400

    добавлен метод readConsole(InputStream in)

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,18 +1,22 @@
 import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.nio.charset.Charset;
 
 //4*5/(6-5)*5+6*8+(6*5*(5+4)*8+(11*4-5*5+6/5*5)+44-5)
 public class Main {
     public static void main(String[] args) {
-        String str = null;
-        try (BufferedReader reader = new BufferedReader(new InputStreamReader(System.in, Charset.forName("UTF-8")))) {
-            str = reader.readLine();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        String result = new Calculator().calculate(str);
+        String result = new Calculator().calculate(readConsole(System.in));
         System.out.println(result);
+    }
+
+    public static String readConsole(InputStream in) {
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(in, Charset.forName("UTF-8")))) {
+            return reader.readLine();
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+        return null;
     }
 }
